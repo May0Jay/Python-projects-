051b4c84a2a52eae5707ebb09a3f8620d8276773
May0Jay <mayankj08851@gmail.com>
2026-06-27 19:25:13 +0530

    Add files via upload

--- Problem_03.py
+++ Problem_03.py
@@ -1,9 +1,14 @@
 '''
-3. Check the type of variable assigned using input() function.
+3. Write a program to detect double space in a string
 
 '''
-take = input("Enter: ")
-t = type(take)
-print(t)
+# we are creating an variable which is containing an double space sting 
+a = "Hey mayank mohit  here"
 
-# -----------------------------------------------------x-------------------------------------------------------------
+# creating an variable and feeding an the condition of double_space using the find function
+double_space = a.find("  ")
+
+# this print prints the index number for showig the location of double_space
+print("the double_space is found in index number: ", double_space)
+
+# -------------------------------------------------------x-----------------------------------------------------------------------------------
