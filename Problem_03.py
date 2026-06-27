fdb67fc7be1435e609ff06eaf9ad9e55bbc58cee
May0Jay <mayankj08851@gmail.com>
2026-06-27 19:22:43 +0530

    Add files via upload

--- Problem_03.py
+++ Problem_03.py
@@ -1,15 +1,9 @@
 '''
-3. Install an external module and use it to perform an operation of your interest.
+3. Check the type of variable assigned using input() function.
 
 '''
-# Step-01 : install the pyjokes using pip function 
-# Step-02 : import pyjokes using import function 
-import pyjokes
+take = input("Enter: ")
+t = type(take)
+print(t)
 
-# using the function you can print the jokes 
-jokes = pyjokes.get_joke()
-
-# Printing the variable 
-print("Have a laugh :",jokes)
-
-# ----------------------------------------------------x------------------------------------------------------------------------------------------
+# -----------------------------------------------------x-------------------------------------------------------------
